65818b13b92e9f5b9f41d43d98cf16de3d3f2b65
Peter Markley <peter@petermarkley.com>
2025-09-22 00:25:05 -0400

    Refactor GlobalId trait

--- src/Traits/HasGlobalId.php
+++ src/Traits/HasGlobalId.php
@@ -4,64 +4,101 @@ namespace PeterMarkley\Tollerus\Traits;
 
 trait HasGlobalId
 {
-    /**
-     * RFC 4648 https://datatracker.ietf.org/doc/html/rfc4648
-     * Safe charset of §5 filename-safe format
-     */
-    const GLOBAL_ID_SAFE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-    const GLOBAL_ID_MAX_SIZE = 16; //maximum number of base64url digits allowed, finite for security reasons
+    public static function isValidGlobalId(string $str): bool
+    {
+        /**
+         * RFC 4648 https://datatracker.ietf.org/doc/html/rfc4648
+         * Check against safe charset of §5 filename-safe format
+         */
+        $pattern = "/^[A-Za-z0-9\-_]+$/";
+        if (!preg_match($pattern, $str)) {
+            return false;
+        }
+
+        // Check length. Maximum is 5 before PHP int overflow
+        if (strlen($str) > 5) {
+            return false;
+        }
+
+        return true;
+    }
 
     private static function GlobalIdEncode(int $num): string
     {
-        $binary = pack("N@3",$num<<8);
+        /**
+         * PHP's normal (signed) integer type has a maximum value of
+         * 2^31-1, because it's 32 bits with one reserved for the sign.
+         * The most base64 digits we can get from that is 5, because
+         * base64 takes 6 bits per digit and 5*6=30.
+         * 
+         * However, the base64 utility works in multiples of 24 bits
+         * (4 base64 digits). So we basically need to inflate to
+         * 48 bits, then trim back down afterward.
+         */
+        // Sanity check input
+        if ($num < 0 || $num > 0x3FFFFFFF) { // 2^30 - 1
+            throw new InvalidArgumentException('GlobalId supports only 0..(2^30-1).');
+        }
 
-        $base64Sect4 = base64_encode($binary);
-        
+        // Read 32 bits
+        $binary32 = pack("N",$num);
+
+        // Inflate to multiple of 24
+        $binary48 = str_pad($binary32, 6, "\0", STR_PAD_LEFT);
+
+        // Encode
+        $encoded = base64_encode($binary48);
+
+        /**
+         * Trim leading 'A's to a minimum of 1. This can be re-padded
+         * later to any desired length.
+         */
+        $trimmed = str_pad(ltrim($encoded, "A"), 1, "A");
+
         /**
          * The PHP function outputs §4 format, but we are
          * using §5 format. So we convert.
          */
-        $base64Sect5 = strtr(
-            rtrim($base64Sect4, '='),
+        $converted = strtr(
+            rtrim($trimmed, '='),
             '+/',
             '-_'
         );
-        
-        return $base64Sect5;
+
+        return $converted;
     }
     
-    private static function GlobalIdDecode(string $base64Sect5): int
+    private static function GlobalIdDecode(string $input): int
     {
         /**
-         * One byte is 8 bits, whereas one base64 character
-         * is 6 bits. Thus the least common multiple is
-         * 12 bits, which is 2 base64 chars or 3 bytes.
-         * 
-         * The pack() utility can only take offsets given in
-         * bytes, so we need to use that LCM here as part of
-         * the conversion process.
+         * Just as above, we need to inflate to 48 bits then trim
+         * back down to 32 after decoding.
          */
-        $base64Multiplied = $base64Sect5."AA";
+        // Sanity check input
+        if (!self::isValidGlobalId($input)) {
+            throw new InvalidArgumentException('GlobalId supports only RFC 4648 §5 characters (url-safe base64), length 1-5.');
+        }
+
+        // We can't use more than 5 input digits (30 bits)
+        $trimmed = substr($input, -5);
+
+        // Inflate to multiple of 4 digits (48 bits)
+        $inflated = str_pad($trimmed, 8, "A", STR_PAD_LEFT);
 
         /**
          * We are using the §5 format, but the PHP function
          * expects §4 format. So we convert.
          */
+        $converted = strtr($inflated, '-_', '+/');
 
-        // Ensure new length is multiple of 4
-        $newLength = ceil(strlen($base64Multiplied)/4)*4;
+        // Decode
+        $binary48 = base64_decode($converted);
 
-        // Pad with '='s and substitute characters
-        $base64Sect4 = str_pad(
-            strtr($base64Multiplied, '-_', '+/'),
-            $newLength,
-            '=',
-            STR_PAD_RIGHT
-        );
+        // Trim to 32 bits
+        $binary32 = substr($binary48, -4);
 
-        $binary = base64_decode($base64Sect4);
-
-        $num = unpack("Nint",$binary)["int"]>>8;
+        // Build integer
+        $num = unpack("Nint",$binary32)["int"];
 
         return $num;
     }
