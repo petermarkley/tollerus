3211353fbda1ef12dc284eccd2297ddfb18e3474
Peter Markley <peter@petermarkley.com>
2025-10-11 14:57:47 -0400

    Move English grammar logic into optional LanguageFactory method

--- src/Database/Factories/LanguageFactory.php
+++ src/Database/Factories/LanguageFactory.php
@@ -41,6 +41,167 @@ class LanguageFactory extends Factory
         });
     }
 
+    /**
+     * This will add WordClassGroups, Features/FeatureValues, and DisplayTables
+     * matching real-life English grammar. Used for testing/demo purposes.
+     */
+    public function withEnglishGrammar(): static
+    {
+        return $this->afterCreating(function (Language $language) {
+            // Adjectives
+            // ----------
+            WordClassGroup::factory()
+                ->for($language)
+                ->has(WordClass::factory()
+                    ->for($language)
+                    ->state(['name'=>'adjective'])
+                )->create();
+
+            // Adverbs
+            // -------
+            WordClassGroup::factory()
+                ->for($language)
+                ->has(WordClass::factory()
+                    ->for($language)
+                    ->state(['name'=>'adverb'])
+                )->create();
+
+            // Verbs
+            // -----
+            $group = WordClassGroup::factory()
+                ->for($language)
+                ->has(WordClass::factory()
+                    ->for($language)
+                    ->state(['name'=>'auxiliary verb'])
+                )->has(WordClass::factory()
+                    ->for($language)
+                    ->state(['name'=>'verb'])
+                )->create(['inflected'=>true]);
+            // Add verb inflection features
+            $verbRole = Feature::factory()->for($group,'group')>create(['name' => 'role']);
+            $verbInfinitive = FeatureValue::factory()->for($verbRole)->create(['name'=>'infinitive']);
+            $verbFinite     = FeatureValue::factory()->for($verbRole)->create(['name'=>'finite']);
+            $verbParticiple = FeatureValue::factory()->for($verbRole)->create(['name'=>'participle']);
+            $verbTense = Feature::factory()->for($group,'group')>create(['name' => 'tense']);
+            $verbPast    = FeatureValue::factory()->for($verbTense)->create(['name'=>'past']);
+            $verbPresent = FeatureValue::factory()->for($verbTense)->create(['name'=>'present', 'name_brief'=>'pres.']);
+            $verbAspect = Feature::factory()->for($group,'group')>create(['name' => 'aspect']);
+            $verbPerfect     = FeatureValue::factory()->for($verbAspect)->create(['name'=>'perfect', 'name_brief'=>'perf.']);
+            $verbSimple      = FeatureValue::factory()->for($verbAspect)->create(['name'=>'simple']);
+            $verbProgressive = FeatureValue::factory()->for($verbAspect)->create(['name'=>'progressive', 'name_brief'=>'prog.']);
+            $verbNumber = Feature::factory()->for($group,'group')>create(['name' => 'number']);
+            $verbSingular = FeatureValue::factory()->for($verbAspect)->create(['name'=>'singular', 'name_brief'=>'sing.']);
+            $verbPlural   = FeatureValue::factory()->for($verbAspect)->create(['name'=>'plural', 'name_brief'=>'pl.']);
+            $verbPerson = Feature::factory()->for($group,'group')>create(['name' => 'person']);
+            $verbFirst  = FeatureValue::factory()->for($verbAspect)->create(['name'=>'first', 'name_brief'=>"1\u{02E2}\u{1D57}"]);
+            $verbSecond = FeatureValue::factory()->for($verbAspect)->create(['name'=>'second', 'name_brief'=>"2\u{207F}\u{1D48}"]);
+            $verbThird  = FeatureValue::factory()->for($verbAspect)->create(['name'=>'third', 'name_brief'=>"3\u{02B3}\u{1D48}"]);
+            // Add verb inflection tables
+            $dispTable = DisplayTable::factory()
+                ->for($group)
+                ->create([
+                    'label' => 'finite verb',
+                    'position' => 0,
+                    'stack' => true,
+                    'align_on_stack' => true,
+                    'table_fold' => false,
+                    'rows_fold' => false
+                ]);
+            $pivot = new DisplayTableFilter([
+                'disp_table_id' => $dispTable->id,
+                'feature_id' => $verbRole->id,
+                'value_id' => $verbFinite->id,
+            ]);
+            $pivot->save();
+            $pivot = new DisplayTableFilter([
+                'disp_table_id' => $dispTable->id,
+                'feature_id' => $verbAspect->id,
+                'value_id' => $verbSimple->id,
+            ]);
+            $pivot->save();
+            $dispTableRow = DisplayTableRow::factory()
+                ->for($dispTable)
+                ->create([
+                    'label' => '3\u{02B3}\u{1D48} pers. pres. sing.',
+                    'label_brief' => '3\u{02B3}\u{1D48} pers. sing.',
+                    'label_long' => 'third person, present, singular',
+                    'position' => 0,
+                ]);
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbTense->id,
+                'value_id' => $verbPresent->id,
+            ]);
+            $pivot->save();
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbPerson->id,
+                'value_id' => $verbThird->id,
+            ]);
+            $pivot->save();
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbNumber->id,
+                'value_id' => $verbSingular->id,
+            ]);
+            $pivot->save();
+            $dispTableRow = DisplayTableRow::factory()
+                ->for($dispTable)
+                ->create([
+                    'label' => 'past tense',
+                    'label_brief' => 'past',
+                    'position' => 1,
+                ]);
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbTense->id,
+                'value_id' => $verbPast->id,
+            ]);
+            $pivot->save();
+            $dispTable = DisplayTable::factory()
+                ->for($group)
+                ->create([
+                    'label' => 'participle',
+                    'position' => 1,
+                    'stack' => true,
+                    'align_on_stack' => false,
+                    'table_fold' => false,
+                    'rows_fold' => false
+                ]);
+            $pivot = new DisplayTableFilter([
+                'disp_table_id' => $dispTable->id,
+                'feature_id' => $verbRole->id,
+                'value_id' => $verbParticiple->id,
+            ]);
+            $pivot->save();
+            $dispTableRow = DisplayTableRow::factory()
+                ->for($dispTable)
+                ->create([
+                    'label' => 'present',
+                    'label_brief' => 'pres.',
+                    'position' => 0,
+                ]);
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbAspect->id,
+                'value_id' => $verbProgressive->id,
+            ]);
+            $pivot->save();
+            $dispTableRow = DisplayTableRow::factory()
+                ->for($dispTable)
+                ->create([
+                    'label' => 'past',
+                    'position' => 1,
+                ]);
+            $pivot = new DisplayTableRowFilter([
+                'disp_table_row_id' => $dispTableRow->id,
+                'feature_id' => $verbAspect->id,
+                'value_id' => $verbPast->id,
+            ]);
+            $pivot->save();
+        });
+    }
+
     protected static function generateName(): array
     {
         $prefixArray = [
