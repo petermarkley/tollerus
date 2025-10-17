5a11b0a894ad4667116a1f8a82edb8ee2e105ee3
Peter Markley <peter@petermarkley.com>
2025-10-16 23:59:41 -0400

    Basic form of AutoInflector class

---
+++ src/Domain/Morphology/Services/AutoInflector.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PeterMarkley\Tollerus\Domain\Morphology\Services;
+
+use PeterMarkley\Tollerus\Domain\Morphology\DTO\AutoInflectorInput;
+
+final class AutoInflector
+{
+    /**
+     * Returns an inflected suggestion
+     */
+    public static function suggest(AutoInflectorInput $input): string
+    {
+        $newBase     = self::batchReplace($input->base, $input->baseRegExs);
+        $newParticle = self::batchReplace($input->particle, $input->particleRegExs);
+        return self::applyTemplate($newBase, $newParticle, $input->template);
+    }
+
+    /**
+     * Perform a set of cumulative RegEx replacements on a string
+     */
+    private static function batchReplace(
+        string $subject,
+        array $regExArgs,
+    ): string
+    {
+        return collect($regExArgs)->reduce(function ($carry, $args) {
+            try {
+                $output = preg_replace(
+                    $args['pattern'],
+                    $args['replacement'],
+                    $carry
+                ) ?? $carry;
+            } catch (\Exception $e) {
+                // Errors should pass silently
+                $output = $carry;
+            }
+            return $output;
+        }, $subject);
+    }
+
+    /**
+     * Apply the template to receive the final inflected word form
+     */
+    private static function applyTemplate(
+        string $base,
+        string $particle,
+        string $template,
+    ): string
+    {
+        // FIXME - this is dumb but works as a placeholder for now
+        return $base . $particle;
+    }
+}
