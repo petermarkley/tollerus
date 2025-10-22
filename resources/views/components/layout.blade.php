30ef76a615bac56150d1abe28c3aa1e19ab815da
Peter Markley <peter@petermarkley.com>
2025-10-22 14:39:03 -0400

    Use 'pointer-events-none' on background layer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="{{ asset('vendor/tollerus/tollerus.css') }}">
     </head>
     <body>
-        <div class="w-full h-full absolute -z-10 bg-gradient-to-b from-zinc-300 to-zinc-400 bg-repeat-x mask-[url(/vendor/tollerus/bg.svg)] mask-size-[400px] mask-alpha mask-repeat dark:from-zinc-800 dark:to-zinc-800 opacity-50 dark:opacity-100" role="none" aria-hidden="true"></div>
+        <div class="w-full h-full absolute -z-10 pointer-events-none bg-gradient-to-b from-zinc-300 to-zinc-400 bg-repeat-x mask-[url(/vendor/tollerus/bg.svg)] mask-size-[400px] mask-alpha mask-repeat dark:from-zinc-800 dark:to-zinc-800 opacity-50 dark:opacity-100" role="none" aria-hidden="true"></div>
         <header class="py-4 mb-4 border-b-2 border-zinc-400 dark:border-zinc-700">
             <x-tollerus::logo.color class="h-18 block dark:hidden mx-auto"/>
             <x-tollerus::logo.color light class="h-18 hidden dark:block mx-auto"/>
