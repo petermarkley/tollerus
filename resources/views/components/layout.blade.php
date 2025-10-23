edcee6feb17fcdce0af65e0aa6323ffe1017f024
Peter Markley <peter@petermarkley.com>
2025-10-22 20:39:05 -0400

    Fix some style issues, add footer in layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
-<html class="bg-gradient-to-b from-zinc-300 to-zinc-300 text-zinc-900 bg-repeat-x dark:from-zinc-800 dark:to-zinc-900 dark:text-zinc-300 w-full h-full">
+<html class="w-full h-full min-h-screen bg-zinc-300 dark:bg-800 text-zinc-900 dark:text-zinc-300">
     <head>
         <title>{{ $title }}</title>
         <link rel="stylesheet" href="{{ asset('vendor/tollerus/tollerus.css') }}">
     </head>
-    <body>
+    <body class="bg-gradient-to-b from-zinc-300 to-zinc-300 bg-repeat-x dark:from-zinc-800 dark:to-zinc-900 relative -z-20">
         <div class="w-full h-full absolute -z-10 pointer-events-none bg-gradient-to-b from-zinc-300 to-zinc-400 bg-repeat-x mask-[url(/vendor/tollerus/bg.svg)] mask-size-[400px] mask-alpha mask-repeat dark:from-zinc-800 dark:to-zinc-800 opacity-50 dark:opacity-100" role="none" aria-hidden="true"></div>
-        <header class="py-4 mb-4 border-b-2 border-zinc-400 dark:border-zinc-700">
-            <x-tollerus::logo.color class="h-18 block dark:hidden mx-auto"/>
-            <x-tollerus::logo.color light class="h-18 hidden dark:block mx-auto"/>
-        </header>
-        <main class="mx-10">
-            {{ $slot }}
-        </main>
+        <div class="flex flex-col gap-4 w-full items-stretch h-full min-h-screen">
+            <header class="w-full py-2 bg-zinc-300 dark:bg-zinc-800 shadow">
+                <div class="md:max-w-[1200px] mx-auto px-6 md:px-0">
+                    <x-tollerus::logo.mono class="h-6 block dark:hidden text-zinc-700"/>
+                    <x-tollerus::logo.mono light class="h-6 hidden dark:block"/>
+                </div>
+            </header>
+            <main class="w-full md:max-w-[1200px] mx-auto px-6 md:px-0 flex-grow">
+                {{ $slot }}
+            </main>
+            <footer class="w-full md:max-w-[1200px] mx-auto px-6 md:px-0 text-center text-zinc-800 dark:text-zinc-500 pb-8">
+                <p>The <a href="https://github.com/petermarkley/tollerus" class="text-cyan-800 dark:text-cyan-500 opacity-75 saturate-80 dark:saturate-20">Tollerus software</a> is copyright &copy; {{ date('Y') }} by Peter Markley.<br>Licensed via <a href="https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html" class="text-cyan-800 dark:text-cyan-500 opacity-75 saturate-80 dark:saturate-20">LGPL v2.1</a></p>
+            </footer>
+        </div>
     </body>
 </html>
