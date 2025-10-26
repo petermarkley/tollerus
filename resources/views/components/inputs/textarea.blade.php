92526a29f4481df69c0812b4edcd8127b23a395e
Peter Markley <peter@petermarkley.com>
2025-10-26 19:31:13 -0400

    Components shouldn't rely on presence of '$dirty'

--- resources/views/components/inputs/textarea.blade.php
+++ resources/views/components/inputs/textarea.blade.php
@@ -5,7 +5,7 @@
 ])
 <div class="flex flex-col gap-1 items-start">
     <label for="{{ $id }}">{{ $label }}</label>
-    <textarea id="{{ $id }}" wire:model.defer="{{ $model }}" rows="10" wire:change="$set('dirty', true)" class="border p-2 w-full rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 @error($model) border-red-700 dark:border-red-500 @else border-zinc-400 dark:border-zinc-600 @enderror"></textarea>
+    <textarea id="{{ $id }}" wire:model.defer="{{ $model }}" rows="10" {{ $attributes }} class="border p-2 w-full rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 @error($model) border-red-700 dark:border-red-500 @else border-zinc-400 dark:border-zinc-600 @enderror"></textarea>
     @error($model)
         <p class="text-red-700 dark:text-red-500 text-sm">{{ $message }}</p>
     @enderror
--- resources/views/livewire/language-editor.blade.php
+++ resources/views/livewire/language-editor.blade.php
@@ -6,22 +6,22 @@
         @endif
     </h1>
     <x-tollerus::panel class="flex flex-col gap-6">
-        <x-tollerus::inputs.toggle id="visible" model="form.visible" label="{{ __('tollerus::ui.visible') }}" :checked="$form['visible']" />
+        <x-tollerus::inputs.toggle id="visible" model="form.visible" label="{{ __('tollerus::ui.visible') }}" :checked="$form['visible']" wire:change="$set('dirty', true)" />
         <div class="flex flex-col gap-4">
             <h3 class="font-bold text-lg">{{ __('tollerus::ui.name') }}</h3>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <x-tollerus::inputs.text id="name" model="form.name" label="{{ __('tollerus::ui.human_friendly') }}" />
-                <x-tollerus::inputs.text id="machine_name" model="form.machine_name" label="{{ __('tollerus::ui.machine_friendly') }}" />
+                <x-tollerus::inputs.text id="name" model="form.name" label="{{ __('tollerus::ui.human_friendly') }}" wire:change="$set('dirty', true)" />
+                <x-tollerus::inputs.text id="machine_name" model="form.machine_name" label="{{ __('tollerus::ui.machine_friendly') }}" wire:change="$set('dirty', true)" />
             </div>
         </div>
         <div class="flex flex-col gap-4">
             <h3 class="font-bold text-lg">{{ __('tollerus::ui.dictionary_info') }}</h3>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <x-tollerus::inputs.text id="dict_title" model="form.dict_title" label="{{ __('tollerus::ui.title_short') }}" />
-                <x-tollerus::inputs.text id="dict_title_full" model="form.dict_title_full" label="{{ __('tollerus::ui.title_full') }}" />
+                <x-tollerus::inputs.text id="dict_title" model="form.dict_title" label="{{ __('tollerus::ui.title_short') }}" wire:change="$set('dirty', true)" />
+                <x-tollerus::inputs.text id="dict_title_full" model="form.dict_title_full" label="{{ __('tollerus::ui.title_full') }}" wire:change="$set('dirty', true)" />
             </div>
-            <x-tollerus::inputs.text id="dict_author" model="form.dict_author" label="{{ __('tollerus::ui.author') }}" />
-            <x-tollerus::inputs.textarea id="intro" model="form.intro" label="{{ __('tollerus::ui.intro') }}" />
+            <x-tollerus::inputs.text id="dict_author" model="form.dict_author" label="{{ __('tollerus::ui.author') }}" wire:change="$set('dirty', true)" />
+            <x-tollerus::inputs.textarea id="intro" model="form.intro" label="{{ __('tollerus::ui.intro') }}" wire:change="$set('dirty', true)" />
         </div>
         <div>
             <button wire:click="save" @disabled(!$dirty) wire:loading.attr="disabled" class="bg-cyan-800 dark:bg-cyan-500 hover:bg-cyan-700 hover:dark:bg-cyan-400 text-white dark:text-zinc-950 saturate-50 font-bold cursor-pointer rounded-lg py-2 px-4 shadow disabled:cursor-not-allowed disabled:bg-zinc-500 disabled:dark:bg-zinc-400 disabled:saturate-100">Save</button>
