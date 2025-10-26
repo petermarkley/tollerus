39ef12e7812ea0857d7022cdcf6733d2be2d8c49
Peter Markley <peter@petermarkley.com>
2025-10-26 16:12:28 -0400

    Style tweaks

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -3,10 +3,10 @@
   'label' => '',
   'model' => '',
 ])
-<div class="flex flex-col gap-1">
+<div class="flex flex-col gap-1 items-start">
     <label for="{{ $id }}">{{ $label }}</label>
-    <input type="text" id="{{ $id }}" wire:model.defer="{{ $model }}" class="border p-2 rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 @error($model) border-red-800 dark:border-red-500 @else border-zinc-400 dark:border-zinc-600 @enderror">
+    <input type="text" id="{{ $id }}" wire:model.defer="{{ $model }}" class="border p-2 w-full rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 @error($model) border-red-700 dark:border-red-500 @else border-zinc-400 dark:border-zinc-600 @enderror">
     @error($model)
-        <p class="text-red-600 text-sm">{{ $message }}</p>
+        <p class="text-red-700 dark:text-red-500 text-sm">{{ $message }}</p>
     @enderror
 </div>
