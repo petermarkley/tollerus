16339f0b566eb8d24c6b362cc47efc0bba9517fb
Peter Markley <peter@petermarkley.com>
2025-11-01 01:58:23 -0400

    Fix misc style/UI issues

--- resources/views/livewire/language-editor.blade.php
+++ resources/views/livewire/language-editor.blade.php
@@ -245,13 +245,13 @@
 
         {{-- GRAMMAR TAB --}}
         <x-tollerus::panel id="tabpanel-grammar" role="tabpanel" x-cloak x-show="tab=='grammar'" class="flex flex-col gap-4 items-start">
-            <div x-cloak x-show="grammarForm.length == 0" class="flex flex-col gap-4 items-center w-full" x-data="{ btn: 'load' }">
+            <div x-cloak x-show="grammarForm.length == 0" class="flex flex-col gap-4 items-center w-full" x-data="{ btn: 'load', preset: '' }">
                 <x-tollerus::alert>
                     <p>{{ __('tollerus::ui.preset_notice') }}</p>
                 </x-tollerus::alert>
                 <div class="flex flex-row gap-4 justify-start items-center">
-                    <x-tollerus::inputs.select id="preset" :options="$presets" label="{{ __('tollerus::ui.preset') }}"/>
-                    <x-tollerus::inputs.button x-text="msgs[btn]" />
+                    <x-tollerus::inputs.select id="preset" :options="$presets" label="{{ __('tollerus::ui.preset') }}" model="preset"/>
+                    <x-tollerus::inputs.button x-text="msgs[btn]" x-bind:disabled="preset.length == 0" />
                 </div>
             </div>
             <div class="flex flex-col gap-6 items-center w-full">
