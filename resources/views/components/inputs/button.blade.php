897184cc74506451b595eb2bf93dcddd7000bb51
Peter Markley <peter@petermarkley.com>
2025-11-06 02:16:37 -0500

    Increase accent saturation in light mode

--- resources/views/components/inputs/button.blade.php
+++ resources/views/components/inputs/button.blade.php
@@ -6,10 +6,10 @@
     @case('primary')
         @switch($size)
             @case('medium')
-                <button {{ $attributes->merge(['class' => 'bg-cyan-800 dark:bg-cyan-500 hover:bg-cyan-700 hover:dark:bg-cyan-400 text-white dark:text-zinc-950 saturate-50 font-bold cursor-pointer rounded-lg py-2 px-4 shadow disabled:cursor-not-allowed disabled:font-normal disabled:bg-zinc-300 disabled:dark:bg-zinc-600 disabled:saturate-100']) }}>{{ $slot }}</button>
+                <button {{ $attributes->merge(['class' => 'bg-cyan-800 dark:bg-cyan-500 hover:bg-cyan-700 hover:dark:bg-cyan-400 text-white dark:text-zinc-950 dark:saturate-50 font-bold cursor-pointer rounded-lg py-2 px-4 shadow disabled:cursor-not-allowed disabled:font-normal disabled:bg-zinc-300 disabled:dark:bg-zinc-600 disabled:saturate-100']) }}>{{ $slot }}</button>
             @break;
             @case('small')
-                <button {{ $attributes->merge(['class' => 'bg-cyan-800 dark:bg-cyan-500 hover:bg-cyan-700 hover:dark:bg-cyan-400 text-white dark:text-zinc-950 saturate-50 font-bold cursor-pointer rounded-lg p-1 shadow disabled:cursor-not-allowed disabled:font-normal disabled:bg-zinc-300 disabled:dark:bg-zinc-600 disabled:saturate-100']) }}>{{ $slot }}</button>
+                <button {{ $attributes->merge(['class' => 'bg-cyan-800 dark:bg-cyan-500 hover:bg-cyan-700 hover:dark:bg-cyan-400 text-white dark:text-zinc-950 dark:saturate-50 font-bold cursor-pointer rounded-lg p-1 shadow disabled:cursor-not-allowed disabled:font-normal disabled:bg-zinc-300 disabled:dark:bg-zinc-600 disabled:saturate-100']) }}>{{ $slot }}</button>
             @break;
         @endswitch
     @break;
