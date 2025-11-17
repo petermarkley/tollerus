2bbd396c9ecce50baa2103c34bfbc01bb4693341
Peter Markley <peter@petermarkley.com>
2025-11-16 22:01:28 -0500

    Remove unused translation keys

--- lang/en/error.php
+++ lang/en/error.php
@@ -12,18 +12,11 @@ return [
     */
     'invalid_preset' => 'Invalid preset',
     'invalid_word_class' => 'Invalid word class ID',
-    'duplicate_word_class_name' => 'Word class name must be unique per group.',
-    'duplicate_word_class_name_brief' => 'Word class name abbreviation must be unique per group.',
     'invalid_word_class_group' => 'Invalid word class group ID',
     'invalid_feature' => 'Invalid feature ID',
-    'duplicate_feature_name' => 'Feature name must be unique per group.',
-    'duplicate_feature_name_brief' => 'Feature name abbreviation must be unique per group.',
     'invalid_feature_value' => 'Invalid feature value ID',
-    'duplicate_feature_value_name' => 'Value name must be unique per group.',
-    'duplicate_feature_value_name_brief' => 'Value name abbreviation must be unique per group.',
     'duplicate_of_unique' => 'This must be unique per group.',
     'invalid_inflection_table' => 'Invalid inflection table ID',
     'invalid_prop_name' => 'Invalid property name',
-    'duplicate_inflection_table_label' => 'Table label must be unique per group.',
     'max_attempts_adding_unique_name' => 'Reached max attempts while trying to create unique object name.',
 ];
