d59d969e962b88e6bab234557327055ab6d19c2b
Peter Markley <peter@petermarkley.com>
2026-01-03 14:48:39 -0500

    Un-break pane component

--- resources/views/components/pane.blade.php
+++ resources/views/components/pane.blade.php
@@ -1,5 +1,6 @@
 @props([
-    'withPadding' => false,
+    'withPadding' => true,
+    'href' => '',
 ])
 @php
     $pStyle = '';
@@ -7,13 +8,13 @@
         $pStyle = 'p-4 ';
     }
 @endphp
-@if (isset($href))
-    <a {{ $attributes->merge(['class' => 'block relative group text-zinc-900 dark:text-zinc-300 rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 cursor-pointer hover:opacity-70']) }}>
+@if ($href)
+    <a {{ $attributes->merge(['class' => $pStyle.'block relative group text-zinc-900 dark:text-zinc-300 rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30 cursor-pointer hover:opacity-70']) }}>
         {{ $slot }}
         <div class="absolute inset-0 w-full h-full rounded-lg border border-cyan-400 dark:border-cyan-700 z-10 pointer-events-none bg-cyan-200/10 dark:bg-cyan-700/10 opacity-0 group-hover:opacity-100"></div>
     </a>
 @else
-    <div {{ $attributes->merge(['class' => 'rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30']) }}>
+    <div {{ $attributes->merge(['class' => $pStyle.'rounded-lg inset-shadow-sm bg-zinc-50 dark:bg-zinc-900/30']) }}>
         {{ $slot }}
     </div>
 @endif
