ed87eb8ba66eddcd4e0028e1723d9f8b5533c683
Peter Markley <peter@petermarkley.com>
2026-01-21 17:52:41 -0500

    Make canonical tab optional on phonemic keyboard; dereference 'tab' component for editing

--- resources/views/components/keyboards/phonemic.blade.php
+++ resources/views/components/keyboards/phonemic.blade.php
@@ -1,62 +1,79 @@
 @props([
     'phonemicKeyboard',
+    'showCanonical' => true,
 ])
 <template id="phonemic_keyboard">
-    <div class="flex flex-col gap-4 px-6" x-data="{ phonemicTab: 'canonical' }">
-        <ul class="px-4 flex flex-row gap-4 justify-start items-end" role="tablist">
-            <x-tollerus::inputs.tab
-                switcher="phonemicTab"
-                tabName="canonical"
-                aria-controls="tabpanel-canonical"
-                title="{{ __('tollerus::ui.canonical') }}"
-                @click="phonemicTab = 'canonical';"
-                @keydown.enter.prevent="phonemicTab = 'canonical';"
-                @keydown.space.prevent="phonemicTab = 'canonical';"
-            >
-                <span class="sr-only md:not-sr-only">{{ __('tollerus::ui.canonical') }}</span>
-            </x-tollerus::inputs.tab>
+    <div class="flex flex-col gap-4 px-6" x-data="{ phonemicTab: '{{ (filter_var($showCanonical, FILTER_VALIDATE_BOOLEAN) ? 'canonical' : 'consonants') }}' }">
+        <ul class="px-4 flex flex-row flex-wrap md:flex-nowrap gap-4 justify-start items-end" role="tablist">
+            @if (filter_var($showCanonical, FILTER_VALIDATE_BOOLEAN))
+                <li
+                    role="tab"
+                    x-bind:aria-selected="phonemicTab == 'canonical'"
+                    tabindex="0"
+                    aria-controls="tabpanel-canonical"
+                    title="{{ __('tollerus::ui.canonical') }}"
+                    @click="phonemicTab = 'canonical';"
+                    @keydown.enter.prevent="phonemicTab = 'canonical';"
+                    @keydown.space.prevent="phonemicTab = 'canonical';"
+                    x-bind:class="{
+                        'rounded-t-lg flex flex-row justify-start items-center gap-2 cursor-pointer py-2 px-4 flex focus:outline-2 outline-offset-2 outline-blue-700 dark:outline-white': true,
+                        'bg-zinc-50 dark:bg-zinc-900 hover:bg-white hover:dark:bg-zinc-800': phonemicTab!='canonical',
+                        'bg-white dark:bg-zinc-800 hover:bg-zinc-50 hover:dark:bg-zinc-700': phonemicTab=='canonical'
+                    }"
+                >
+                    <span>{{ __('tollerus::ui.canonical') }}</span>
+                </li>
+            @endif
             @foreach ($phonemicKeyboard['tabs'] as $tab)
-                <x-tollerus::inputs.tab
-                    switcher="phonemicTab"
-                    tabName="{{ $tab['key'] }}"
+                <li
+                    role="tab"
+                    x-bind:aria-selected="phonemicTab == '{{ $tab['key'] }}'"
+                    tabindex="0"
                     aria-controls="tabpanel-{{ $tab['key'] }}"
                     title="{{ $tab['label'] }}"
                     @click="phonemicTab = '{{ $tab['key'] }}';"
                     @keydown.enter.prevent="phonemicTab = '{{ $tab['key'] }}';"
                     @keydown.space.prevent="phonemicTab = '{{ $tab['key'] }}';"
+                    x-bind:class="{
+                        'rounded-t-lg flex flex-row justify-start items-center gap-2 cursor-pointer py-2 px-4 flex focus:outline-2 outline-offset-2 outline-blue-700 dark:outline-white': true,
+                        'bg-zinc-50 dark:bg-zinc-900 hover:bg-white hover:dark:bg-zinc-800': phonemicTab!='{{ $tab['key'] }}',
+                        'bg-white dark:bg-zinc-800 hover:bg-zinc-50 hover:dark:bg-zinc-700': phonemicTab=='{{ $tab['key'] }}'
+                    }"
                 >
-                    <span class="sr-only md:not-sr-only">{{ $tab['label'] }}</span>
-                </x-tollerus::inputs.tab>
+                    <span>{{ $tab['label'] }}</span>
+                </li>
             @endforeach
         </ul>
-        <div
-            id="tabpanel-canonical"
-            role="tabpanel"
-            x-cloak x-show="phonemicTab=='canonical'"
-            class="w-full grid grid-cols-20 gap-1"
-        >
-            @foreach ($phonemicKeyboard['canonical'] as $glyph)
-                <div class="w-full @container">
-                    <button
-                        @class([
-                            'w-full flex flex-col justify-between items-center bg-white dark:bg-zinc-800 rounded-[20cqw] shadow/40 hover:shadow-lg/20 focus:shadow-lg/20 active:shadow-sm/80 p-1 border border-b-[10cqw] border-zinc-400 dark:border-zinc-600 hover:bg-zinc-100 cursor-pointer hover:dark:bg-zinc-700',
-                            'hover:transform-[translateY(-6cqw)] focus:transform-[translateY(-6cqw)] active:transform-[translateY(6cqw)]',
-                        ])
-                        data-glyph="{{ $glyph->glyph }}"
-                        title="{{ $glyph->labelTranslated }}"
-                        @click="$store.phonemicKeyboard.click"
-                    >
-                        <span class="sr-only" @if(!$glyph->recognized) lang="en" @endif>{{ $glyph->labelTranslated }}</span>
-                        @if ($glyph->render_on_base)
-                            <span class="text-[60cqw]">&#x25CC;{{ $glyph->glyph }}</span>
-                        @else
-                            <span class="text-[60cqw]">{{ ($glyph->glyph==' '? '&nbsp;' : $glyph->glyph) }}</span>
-                        @endif
-                        <span class="text-[15cqw] font-mono text-zinc-500 dark:text-zinc-500 line-clamp-1">{{ $glyph->hex }}</span>
-                    </button>
-                </div>
-            @endforeach
-        </div>
+        @if (filter_var($showCanonical, FILTER_VALIDATE_BOOLEAN))
+            <div
+                id="tabpanel-canonical"
+                role="tabpanel"
+                x-cloak x-show="phonemicTab=='canonical'"
+                class="w-full grid grid-cols-20 gap-1"
+            >
+                @foreach ($phonemicKeyboard['canonical'] as $glyph)
+                    <div class="w-full @container">
+                        <button
+                            @class([
+                                'w-full flex flex-col justify-between items-center bg-white dark:bg-zinc-800 rounded-[20cqw] shadow/40 hover:shadow-lg/20 focus:shadow-lg/20 active:shadow-sm/80 p-1 border border-b-[10cqw] border-zinc-400 dark:border-zinc-600 hover:bg-zinc-100 cursor-pointer hover:dark:bg-zinc-700',
+                                'hover:transform-[translateY(-6cqw)] focus:transform-[translateY(-6cqw)] active:transform-[translateY(6cqw)]',
+                            ])
+                            data-glyph="{{ $glyph->glyph }}"
+                            title="{{ $glyph->labelTranslated }}"
+                            @click="$store.phonemicKeyboard.click"
+                        >
+                            <span class="sr-only" @if(!$glyph->recognized) lang="en" @endif>{{ $glyph->labelTranslated }}</span>
+                            @if ($glyph->render_on_base)
+                                <span class="text-[60cqw]">&#x25CC;{{ $glyph->glyph }}</span>
+                            @else
+                                <span class="text-[60cqw]">{{ ($glyph->glyph==' '? '&nbsp;' : $glyph->glyph) }}</span>
+                            @endif
+                            <span class="text-[15cqw] font-mono text-zinc-500 dark:text-zinc-500 line-clamp-1">{{ $glyph->hex }}</span>
+                        </button>
+                    </div>
+                @endforeach
+            </div>
+        @endif
         @foreach ($phonemicKeyboard['tabs'] as $tab)
             @php
                 $height = collect($tab['glyphs'])->max('row');
