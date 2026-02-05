674c0d5b036e5c0b3f413bd87a4524213efdb87e
Peter Markley <peter@petermarkley.com>
2026-02-05 13:00:11 -0500

    Get customizable CSS vars actually working on public page

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,3 +1,3 @@
 <x-tollerus::layouts.public :title="$title ?? 'Dictionary'">
-    <div>Lorem ipsum doooooolor sit amet.</div>
+    <div class="mx-auto mt-4 bg-tollerus-surface hover:bg-tollerus-surface-hover rounded-lg shadow-lg p-6">Lorem ipsum doooooolor sit amet.</div>
 </x-tollerus::layouts.public>
