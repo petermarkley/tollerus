f9bb33934e93fe4fdd65d9d4d3d1aadfdee908c9
Peter Markley <peter@petermarkley.com>
2026-02-08 15:37:22 -0500

    Add icons and responsive/accessible considerations to public nav-main

--- resources/views/components/public/nav-main.blade.php
+++ resources/views/components/public/nav-main.blade.php
@@ -4,16 +4,30 @@
 ])
 @php
     $opts = [
-        'word_lookup' => ['href' => route('tollerus.public.index'), 'text' => __('tollerus::ui.word_lookup')],
-        'language_info' => ['href' => route('tollerus.public.languages.index'), 'text' => trans_choice('tollerus::ui.language_info', $langCount)],
+        'word_lookup' => [
+            'href' => route('tollerus.public.index'),
+            'text' => __('tollerus::ui.word_lookup'),
+            'icon' => 'entries',
+        ],
+        'language_info' => [
+            'href' => route('tollerus.public.languages.index'),
+            'text' => trans_choice('tollerus::ui.language_info', $langCount),
+            'icon' => 'info',
+        ],
     ];
 @endphp
-<div class="flex flex-row gap-4 items-center justify-center">
+<div class="w-full flex flex-row gap-4 items-center justify-center">
     @foreach ($opts as $key => $opt)
         @if ($currentPage == $key)
-            <div class="inline-block bg-tollerus-surface cursor-default rounded-lg p-4 text-tollerus-text font-bold opacity-50">{{ $opt['text'] }}</div>
+            <div title="{{ $opt['text'] }}" class="flex flex-row gap-2 items-center bg-tollerus-surface cursor-default rounded-lg px-6 py-3 text-tollerus-text font-bold opacity-50">
+                <x-dynamic-component :component="'tollerus::icons.' . $opt['icon']" />
+                <span class="sr-only md:not-sr-only">{{ $opt['text'] }}</span>
+            </div>
         @else
-            <a href="{{ $opt['href'] }}" class="inline-block bg-tollerus-surface hover:bg-tollerus-surface-hover cursor-pointer rounded-lg shadow-lg p-4 text-tollerus-text font-bold">{{ $opt['text'] }}</a>
+            <a href="{{ $opt['href'] }}" title="{{ $opt['text'] }}" class="flex flex-row gap-2 items-center bg-tollerus-surface hover:bg-tollerus-surface-hover cursor-pointer rounded-lg shadow-lg px-6 py-3 text-tollerus-text font-bold">
+                <x-dynamic-component :component="'tollerus::icons.' . $opt['icon']" />
+                <span class="sr-only md:not-sr-only">{{ $opt['text'] }}</span>
+            </a>
         @endif
     @endforeach
 </div>
