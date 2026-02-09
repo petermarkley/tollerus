cc90c3ae22f254d2f02bb980dab8aa2f26a8d7a4
Peter Markley <peter@petermarkley.com>
2026-02-08 23:38:29 -0500

    New pagination links view for public page

--- resources/views/public/languages/entries.blade.php
+++ resources/views/public/languages/entries.blade.php
@@ -5,22 +5,24 @@
             <x-tollerus::breadcrumbs :breadcrumbs="$breadcrumbs" isPublic="true"/>
         @endif
         <div class="w-full flex flex-col gap-4 items-start bg-tollerus-surface rounded-lg shadow-lg p-6 text-tollerus-text">
-            <h2 class="text-2xl font-bold">{{ __('tollerus::ui.all_entries_for_language', ['lang' => $language->name]) }}</h2>
-            <div class="w-full flex flex-row gap-4 items-center justify-center">
-                <a
-                    href="#"
-                    class="relative flex flex-row gap-2 items-center cursor-pointer px-4 py-2 rounded-lg shadow font-bold bg-tollerus-secondary hover:bg-tollerus-secondary-hover text-tollerus-text-inverse"
-                >
-                    <x-tollerus::icons.bars-arrow-down/>
-                    <span>{{ __('tollerus::ui.sort_by_transliterated', ['transliterated' => config('tollerus.local_transliteration_target', __('tollerus::ui.transliterated'))]) }}</span>
-                </a>
-                <a
-                    href="#"
-                    class="relative flex flex-row gap-2 items-center cursor-pointer px-4 py-2 rounded-lg shadow font-bold bg-tollerus-secondary hover:bg-tollerus-secondary-hover text-tollerus-text-inverse"
-                >
-                    <x-tollerus::icons.bars-arrow-down/>
-                    <span>{{ __('tollerus::ui.sort_by_native') }}</span>
-                </a>
+            <div class="w-full flex flex-col md:flex-row gap-4 items-stretch md:items-center">
+                <h2 class="text-2xl font-bold">{{ __('tollerus::ui.all_entries_for_language', ['lang' => $language->name]) }}</h2>
+                <div class="flex flex-row gap-4 items-center justify-center">
+                    <a
+                        href="#"
+                        class="relative flex flex-row gap-2 items-center cursor-pointer px-4 py-2 rounded-lg shadow font-bold bg-tollerus-secondary hover:bg-tollerus-secondary-hover text-tollerus-text-inverse"
+                    >
+                        <x-tollerus::icons.bars-arrow-down/>
+                        <span>{{ __('tollerus::ui.sort_by_transliterated', ['transliterated' => config('tollerus.local_transliteration_target', __('tollerus::ui.transliterated'))]) }}</span>
+                    </a>
+                    <a
+                        href="#"
+                        class="relative flex flex-row gap-2 items-center cursor-pointer px-4 py-2 rounded-lg shadow font-bold bg-tollerus-secondary hover:bg-tollerus-secondary-hover text-tollerus-text-inverse"
+                    >
+                        <x-tollerus::icons.bars-arrow-down/>
+                        <span>{{ __('tollerus::ui.sort_by_native') }}</span>
+                    </a>
+                </div>
             </div>
             <div class="w-full p-4 h-auto md:h-200 lg:h-104 flex flex-col justify-start items-start flex-nowrap md:flex-wrap gap-2 border-y-2 border-tollerus-border">
                 @if (count($paginator->items()) > 0)
@@ -47,8 +49,8 @@
                     <span class="italic text-zinc-700 dark:text-zinc-400">{{ __('tollerus::ui.no_results') }}</span>
                 @endif
             </div>
-            <div class="p-4">
-                {{ $paginator->links('tollerus::components.pagination-links', data: ['scrollTo' => false]) }}
+            <div class="w-full">
+                {{ $paginator->links('tollerus::public.languages.pagination-links', data: ['scrollTo' => false]) }}
             </div>
         </div>
     </div>
---
+++ resources/views/public/languages/pagination-links.blade.php
@@ -0,0 +1,68 @@
+<div class="w-full flex flex-row gap-4 justify-center items-center">
+    @if ($paginator->hasPages())
+        <div class="hidden md:block">
+            <p class="italic">{{ __('tollerus::ui.pagination_showing_numbers', [
+                'start' => ($paginator->currentPage()-1)*$paginator->perPage() + 1,
+                'end' => ($paginator->currentPage() == $paginator->lastPage() ? $paginator->total() : $paginator->currentPage()*$paginator->perPage()),
+                'total' => $paginator->total(),
+            ]) }}</p>
+        </div>
+        <nav role="navigation" aria-label="{{ __('tollerus::ui.pagination_navigation') }}" class="flex flex-row gap-2 justify-center items-center">
+            @if ($paginator->onFirstPage())
+                <a title="{{ __('tollerus::ui.first') }}" class="relative text-tollerus-secondary opacity-50 cursor-not-allowed">
+                    <x-tollerus::icons.chevron-double-left class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.first') }}</span>
+                </a>
+                <a title="{{ __('tollerus::ui.previous') }}" class="relative text-tollerus-secondary opacity-50 cursor-not-allowed">
+                    <x-tollerus::icons.chevron-left class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.previous') }}</span>
+                </a>
+            @else
+                <a
+                    rel="prev"
+                    title="{{ __('tollerus::ui.first') }}"
+                    class="relative text-tollerus-secondary hover:text-tollerus-secondary-hover cursor-pointer"
+                >
+                    <x-tollerus::icons.chevron-double-left class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.first') }}</span>
+                </a>
+                <a
+                    rel="prev"
+                    title="{{ __('tollerus::ui.previous') }}"
+                    class="relative text-tollerus-secondary hover:text-tollerus-secondary-hover cursor-pointer"
+                >
+                    <x-tollerus::icons.chevron-left class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.previous') }}</span>
+                </a>
+            @endif
+
+            @if ($paginator->onLastPage())
+                <a title="{{ __('tollerus::ui.next') }}" class="relative text-tollerus-secondary opacity-50 cursor-not-allowed">
+                    <x-tollerus::icons.chevron-right class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.next') }}</span>
+                </a>
+                <a title="{{ __('tollerus::ui.last') }}" class="relative text-tollerus-secondary opacity-50 cursor-not-allowed">
+                    <x-tollerus::icons.chevron-double-right class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.last') }}</span>
+                </a>
+            @else
+                <a
+                    rel="prev"
+                    title="{{ __('tollerus::ui.next') }}"
+                    class="relative text-tollerus-secondary hover:text-tollerus-secondary-hover cursor-pointer"
+                >
+                    <x-tollerus::icons.chevron-right class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.next') }}</span>
+                </a>
+                <a
+                    rel="prev"
+                    title="{{ __('tollerus::ui.last') }}"
+                    class="relative text-tollerus-secondary hover:text-tollerus-secondary-hover cursor-pointer"
+                >
+                    <x-tollerus::icons.chevron-double-right class="w-8 h-8"/>
+                    <span class="sr-only">{{ __('tollerus::ui.last') }}</span>
+                </a>
+            @endif
+        </nav>
+    @endif
+</div>
