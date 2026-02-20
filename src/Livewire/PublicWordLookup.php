e087b435097023569c1d38b671bb44c27b69eff1
Peter Markley <peter@petermarkley.com>
2026-02-20 03:13:07 -0500

    Refactor PublicWordLookup inflection table logic even more for performance

--- src/Livewire/PublicWordLookup.php
+++ src/Livewire/PublicWordLookup.php
@@ -283,46 +283,112 @@ class PublicWordLookup extends Component
                     $group = $lexeme->wordClass->group;
 
                     /**
-                     * Precompute a data lookup for faster matching logic:
+                     * These data lookups facilitate a higher-performance
+                     * matching algorithm once we start processing each
+                     * inflection row. The difference is very noticeable
+                     * in the UI for large inflection tables (50+ forms).
+                     *
+                     * Eloquent model lookup
                      * [
-                     *   <formId> => [
-                     *     <filterId> => 0,
-                     *     <filterId> => 1,
-                     *     ...
-                     *   ],
+                     *   <formId> => <Form::class>,
+                     *   <formId> => <Form::class>,
                      *   ...
                      * ]
-                     * This lets us use `isset()` on a keyed array instead of
-                     * `->contains()` on a collection, which offers _noticeably
-                     * better performance_ when rendering large inflection
-                     * tables (50+ word forms).
                      */
-                    $formInflectionValueIds = $lexeme->forms
-                        ->mapWithKeys(fn ($f) => [$f->id => $f->inflectionValues->pluck('id')->flip()])
+                    $formsById = $lexeme->forms->keyBy('id')->all();
+                    /**
+                     * Cross-lookup from filter values to forms
+                     * [
+                     *   <featureValueId> => [
+                     *     <formId>,
+                     *     <formId>,
+                     *     ...
+                     *   ]
+                     *   ...
+                     * ]
+                     */
+                    $formIdsByValueId = $lexeme->forms
+                        ->flatMap(
+                            fn ($f) => $f->inflectionValues
+                                ->pluck('id')
+                                ->map(fn ($vid) => [$vid => $f->id])
+                        )->mapToGroups(fn ($pair) => $pair)
                         ->toArray();
 
                     $tables = $group->inflectionTables
                         ->where('visible', true)
                         ->sortBy('position')
-                        ->map(function ($table) use ($lexeme, $formInflectionValueIds, $primaryNeography) {
+                        ->map(function ($table) use ($lexeme, $formsById, $formIdsByValueId, $primaryNeography) {
                             $columns = $table->columns
                                 ->where('visible', true)
                                 ->sortBy('position')
-                                ->map(function ($column) use ($lexeme, $formInflectionValueIds, $primaryNeography) {
+                                ->map(function ($column) use ($lexeme, $formsById, $formIdsByValueId, $primaryNeography) {
                                     $rows = $column->rows
                                         ->where('visible', true)
                                         ->sortBy('position')
-                                        ->map(function ($row) use ($column, $lexeme, $formInflectionValueIds, $primaryNeography) {
+                                        ->map(function ($row) use ($column, $lexeme, $formsById, $formIdsByValueId, $primaryNeography) {
                                             $filters = $column->filterValues->concat($row->filterValues);
-                                            $form = $lexeme->forms->filter(
-                                                fn ($form) => $filters->reduce(
-                                                    fn ($carry, $filter) => $carry && isset($formInflectionValueIds[$form->id][$filter->id]),
-                                                    true
-                                                )
-                                            )->first();
-                                            if ($form !== null && $primaryNeography !== null) {
+                                            /**
+                                             * Conceptually we are asking "which forms on this
+                                             * lexeme have [such and such] inflection value?"
+                                             *
+                                             * For example if we're rendering English verbs and this
+                                             * is the row for "3rd pers. sing.", the list of
+                                             * column + row filter values is:
+                                             *
+                                             * [finite, present, simple, 3rd person, singular]
+                                             *
+                                             * So we ask:
+                                             *  1. Which verb forms are finite?
+                                             *  2. Which _of those_ are present tense?
+                                             *  3. Which _of those_ are simple aspect?
+                                             * ... and so on.
+                                             *
+                                             * Thus we whittle the list of forms down as we go. We
+                                             * then render the first form that still remains at the
+                                             * end (if there is one).
+                                             *
+                                             * However we will create an optimized filter list for
+                                             * faster comparison:
+                                             *  - It uses our precomputed cross-lookup
+                                             *  - Allows us to rule out non matches earlier
+                                             */
+                                            $filterIds = $filters->pluck('id')
+                                                ->sortBy(fn ($filterId) => count($formIdsByValueId[$filterId] ?? []))
+                                                ->values();
+                                            /**
+                                             * Implementation using classical imperative syntax ...
+                                             * ====================================================
+                                             */
+                                            // $candidates = null;
+                                            // foreach ($filterIds as $vid) {
+                                            //     $ids = $formIdsByValueId[$vid] ?? [];
+                                            //     $candidates = $candidates === null ? $ids : array_values(array_intersect($candidates, $ids));
+                                            //     if (!$candidates) break;
+                                            // }
+                                            // $formId = $candidates[0] ?? null;
+                                            /**
+                                             * Implementation using declarative Collection syntax ...
+                                             * ======================================================
+                                             */
+                                            $formId = $filterIds->reduce(
+                                                function ($candidates, $filterId) use ($formIdsByValueId) {
+                                                    if ($candidates->isEmpty()) {
+                                                        // Set is empty, so don't call `->intersect()` anymore
+                                                        return $candidates;
+                                                    }
+                                                    // Compare, and whittle down the list of forms ...
+                                                    return $candidates->intersect($formIdsByValueId[$filterId] ?? []);
+                                                },
+                                                // Start with all forms on the lexeme
+                                                collect(array_keys($formsById))
+                                            )->first(); // Done, now pick the first remaining form
+
+                                            if ($formId !== null && $primaryNeography !== null) {
+                                                $form = $formsById[$formId];
                                                 $formNative = $form->nativeSpellings->firstWhere('neography_id', $primaryNeography->id);
                                             } else {
+                                                $form = null;
                                                 $formNative = null;
                                             }
                                             return [
