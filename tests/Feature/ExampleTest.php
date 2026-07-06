052ba26696a412ffccd347109a9fa4ca7e726483
Peter Markley <peter@petermarkley.com>
2026-07-05 20:01:42 -0400

    Use `phpunit.xml.dist` and ignore `phpunit.xml` to protect secrets

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,6 @@
 <?php
 
 it('returns a successful response', function () {
-    dump(route('tollerus.public.index', absolute: false));
     $response = $this->get(route('tollerus.public.index', absolute: false));
 
     $response->assertStatus(200);
