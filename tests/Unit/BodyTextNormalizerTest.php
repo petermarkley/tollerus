dc6adabca9437df98d3a562789635c59cd9d1de9
Peter Markley <peter@petermarkley.com>
2026-07-18 20:39:22 -0400

    tests: full coverage of BodyTextNormalizer

--- tests/Unit/BodyTextNormalizerTest.php
+++ tests/Unit/BodyTextNormalizerTest.php
@@ -46,3 +46,21 @@ it('correctly normalizes HTML for editing', function () {
     $outputHtml = trim(preg_replace('/>\s+</', '><', $outputHtmlRaw));
     expect($outputHtml)->toBe($expectedHtml);
 });
+it('correctly normalizes inline HTML for storage', function () {
+    $normalizer = new BodyTextNormalizer;
+    $inputHtml = "<p>Lorem ipsum dolor sit amet.</p>";
+    $expectedHtmlRaw = "Lorem ipsum dolor sit amet.";
+    $outputHtmlRaw = $normalizer->normalizeInlineForSave($inputHtml);
+    $expectedHtml = trim(preg_replace('/>\s+</', '><', $expectedHtmlRaw));
+    $outputHtml = trim(preg_replace('/>\s+</', '><', $outputHtmlRaw));
+    expect($outputHtml)->toBe($expectedHtml);
+});
+it('correctly normalizes inline HTML for editing', function () {
+    $normalizer = new BodyTextNormalizer;
+    $inputHtml = "Lorem ipsum dolor sit amet.";
+    $expectedHtmlRaw = "<p>Lorem ipsum dolor sit amet.</p>";
+    $outputHtmlRaw = $normalizer->normalizeInlineForWysiwyg($inputHtml);
+    $expectedHtml = trim(preg_replace('/>\s+</', '><', $expectedHtmlRaw));
+    $outputHtml = trim(preg_replace('/>\s+</', '><', $outputHtmlRaw));
+    expect($outputHtml)->toBe($expectedHtml);
+});
